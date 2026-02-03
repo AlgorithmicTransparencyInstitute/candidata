5c2ceef22e944a7a4c4d0eb83defd7d2d43a8b1a
Cameron Hickey <cameronhickey@gmail.com>
2026-02-03 15:07:28 -0500

    Fix HelpController skip_before_action error in production

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class HelpController < ApplicationController
-  skip_before_action :authenticate_user!, if: -> { defined?(authenticate_user!) }
-
   def index
   end
 
