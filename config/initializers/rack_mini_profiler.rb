1dec4eb7a19e8cdd1b6e87c4efcea82e3e6cd88d
Cameron Hickey <cameronhickey@gmail.com>
2026-02-09 08:51:56 -0500

    Configure rack-mini-profiler for performance profiling
    - Enable in development by default
    - Enable in production for admin users only
    - Position in bottom-right corner
    - Use MemoryStore for storing profiling results

--- config/initializers/rack_mini_profiler.rb
+++ config/initializers/rack_mini_profiler.rb
@@ -1,29 +1,45 @@
 # frozen_string_literal: true
 
+# Configuration for rack-mini-profiler
+# Shows performance metrics in development and for admins in production
 if defined?(Rack::MiniProfiler)
-  # Enable in production for admin users only
-  Rack::MiniProfiler.config.authorization_mode = :allow_authorized
+  # Enable in development by default
+  if Rails.env.development?
+    Rack::MiniProfiler.config.tap do |config|
+      # Show badge on all pages
+      config.position = "bottom-right"
 
-  # Determine who is authorized to see the profiler
-  Rack::MiniProfiler.config.pre_authorize_cb = lambda { |env|
-    request = Rack::Request.new(env)
+      # Store profiling results in memory
+      config.storage = Rack::MiniProfiler::MemoryStore
 
-    # Check if user is signed in and is an admin
-    if defined?(Warden) && env['warden']
-      user = env['warden'].user
-      user&.admin?
-    else
-      false
+      # Skip profiling for certain paths if needed
+      # config.skip_paths = ['/admin/sidekiq']
     end
-  }
+  end
 
-  # Position on page (default is top-left, can be top-right, bottom-left, bottom-right)
-  Rack::MiniProfiler.config.position = 'bottom-right'
+  # Enable in production only for admin users
+  if Rails.env.production?
+    Rack::MiniProfiler.config.tap do |config|
+      config.position = "bottom-right"
+      config.storage = Rack::MiniProfiler::MemoryStore
 
-  # Skip paths that don't need profiling
-  Rack::MiniProfiler.config.skip_paths = [
-    '/assets/',
-    '/packs/',
-    '/health_check'
-  ]
+      # Only show profiler for admin users
+      config.authorization_mode = :allow_authorized
+
+      # This method is called before each request to determine if profiling should be enabled
+      # It must return true/false
+      config.pre_authorize_cb = lambda { |env|
+        # Get the current user from the request
+        request = Rack::Request.new(env)
+
+        # Check if user is logged in via Warden (Devise)
+        if defined?(Warden) && env['warden']
+          user = env['warden'].user
+          user&.admin? == true
+        else
+          false
+        end
+      }
+    end
+  end
 end
